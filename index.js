abe293a18e3e373d7c8b859539a5b8e2bb7f404e
SIRREG001 <udochukwureginald78@gmail.com>
2024-03-03 23:39:14 +0100

    adding ejs to application

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var engine = require('ejs-mate')
+const expressLayouts = require('express-ejs-layouts')
+
 
 // var indexRouter = require('./app_server/routes/index');
 // var usersRouter = require('./app_server/routes/users');
 
+//start app
 var app = express();
+app.engine('ejs', engine);
 const PORT = 3000;
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'app_server', 'views'));
-// app.set('view engine', 'jade');
+app.use(expressLayouts)
+app.set('layout', './layouts/boilerplate')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(logger('dev'));
 // app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
@@ -41,11 +50,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Home 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/home.html'));
+    res.render('home', {title: 'Home'})
 })
 //Pitch 
 app.get('/pitch', (req, res)=>{
-    res.sendFile(path.join(__dirname, '/public/pitch.html'));
+    res.render('pitch', {title: 'Pitch'})
 })
 
 // module.exports = app;
